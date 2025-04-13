47fc9a3c5fe831897ab992a0b418ed4a1e3fd075
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2025-04-12 22:06:43 -0400

    added logging to server

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,14 +1,34 @@
-
-from flask import Flask
+import os, logging
+from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Api
 from util.auth import Register, Login, Logout
-
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:8080", supports_credentials=True)
 api = Api(app)
 
+## ===== LOGGING =====
+
+# ensure logs dir exists
+os.makedirs('../logs', exist_ok=True)
+
+# rotating file handler for 5 max files 1MB each
+file_handler = RotatingFileHandler('../logs/server.log', maxBytes=1_000_000, backupCount=5)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s'
+))
+
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+@app.before_request
+def log_request_info():
+    app.logger.info(f"{request.remote_addr} - {request.method} {request.path}")
+
+## ===== ROUTES =====
 api.add_resource(Register, "/register")
 api.add_resource(Login, "/login")
 api.add_resource(Logout, "/logout")
