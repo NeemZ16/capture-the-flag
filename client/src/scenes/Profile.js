2bb26f6b21c5e51dac871434c3d25d6378a9be24
NeemZ16 <110858265+NeemZ16@users.noreply.github.com>
2025-05-04 16:05:55 -0400

    profile scene created and navigation to game working

---
+++ client/src/scenes/Profile.js
@@ -0,0 +1,18 @@
+import { BaseScene } from './BaseScene';
+
+export class Profile extends BaseScene {
+    constructor() {
+        super('Profile');
+    }
+
+    create() {
+        document.getElementById('profile-wrapper').style.display = 'block';
+        document.getElementById('startGame').onclick = () => {
+            document.getElementById('profile-wrapper').style.display = 'none';
+            this.scene.start('Game');
+        };
+
+        
+    }
+
+}
